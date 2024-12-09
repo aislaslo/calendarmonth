4d70376f1fd46c257fae756ef1b3c75e6a3a31b6
aislaslo <islas.alex@gmail.com>
2024-12-09 10:25:16 -0600

    dawn of calendarmonth

---
+++ main.py
@@ -0,0 +1,14 @@
+import calendar
+
+def display_calendar():
+
+    year = int(input("Enter year: "))
+    month = int(input("Enter month (1-12): "))
+
+    cal = calendar.TextCalendar(calendar.MONDAY)
+
+    month_calendar = cal.formatmonth(year, month)
+    print(" ")
+    print(month_calendar)
+
+display_calendar()
